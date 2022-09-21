100ff81fa73c879260f3e3268ab53b785b780e22
Salvins <salvinjose@gmail.com>
2022-09-21 22:15:56 +0530

    End of Section 2

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbcontext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbcontext : DbContext
+    {
+        public NZWalksDbcontext(DbContextOptions<NZWalksDbcontext>options) : base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+
+    }
+}
